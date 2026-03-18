90ea06cf5fcc92e3bc635d265f8fa62a346f4119
dgcavalcante <diogofe788@gmail.com>
2026-03-18 18:35:57 -0300

    creation of the Instrument class and structuring of the main class

---
+++ DjThreads.java
@@ -0,0 +1,6 @@
+public class DjThreads {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Instrumento.java
@@ -0,0 +1,12 @@
+
+public class Instrumento implements Runnable {
+    private String nome;
+    private String batida;
+    private int 
+    
+    
+    @Override
+    public void run(){
+        
+    }
+}
